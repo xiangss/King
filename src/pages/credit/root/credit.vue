50175f9f618643a94d35ca22bdff5a6dc4af6adb
wangtomcat <1217149455@qq.com>
2019-11-12 17:00:50 +0800

    tomtwo

---
+++ src/pages/credit/root/credit.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="credit">
+ 
+</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+#credit{
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 98px;
+  background-color: #fff;
+
+}
+</style>
---
+++ src/pages/home/children/start/Start.vue
@@ -0,0 +1,90 @@
+<template>
+<div id="start">
+    <div class="start" v-if="stratFlag">
+    <img src="../../../assets/1-1.png" alt="">
+  </div>
+  <van-swipe v-if="show" :loop="false" :show-indicators="false" class="content">
+    <van-swipe-item>
+      <img src="../../../assets/1-2.png" alt="">
+    </van-swipe-item>
+    <van-swipe-item>
+      <img src="../../../assets/1-3.png" alt="">
+    </van-swipe-item>
+    <van-swipe-item>
+      <img src="../../../assets/1-4.png" alt="">
+      <span @click="btnAction" class="btn"></span>
+    </van-swipe-item>
+  </van-swipe>
+</div>
+</template>
+
+<script>
+import { Swipe, SwipeItem,Toast } from 'vant'
+export default {
+  components:{
+    [Swipe.name]:Swipe,
+    [SwipeItem.name]:SwipeItem,
+    [Toast.name]:Toast
+  },
+  data(){
+    return{
+      stratFlag:true,
+      show:true
+    }
+  },
+  methods: {
+    btnAction(){ 
+      this.show = false;
+    }
+  },
+  mounted(){
+    if(!(sessionStorage.getItem('startFlag'))){
+      setTimeout(()=>{
+        this.stratFlag = false;
+        sessionStorage.setItem('startFlag',true);
+      },2000)
+    }else{
+      this.stratFlag = false;
+    }
+    if(!(localStorage.getItem('show'))){
+      this.show = true;
+    }else{
+      this.show = false;
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#start{
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0px;
+  z-index: 999;
+  background-color: #fff;
+  .start{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    img{
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .content{
+    width: 100%;
+    height: 100%;
+    img{
+      width: 100%;
+    }
+    .btn{
+      position: absolute;
+      bottom: 120px;
+      width: 500px;
+      height: 130px;
+      left: 240px;
+    }
+  }
+}
+</style>
